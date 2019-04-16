97fb0b751fa346617d804406d66c444be7a09bbe
dinofilipovic <dfilipovic1@comping.hr>
2019-04-16 18:05:03 +0200

    Push DZ

---
+++ index.py
@@ -0,0 +1,44 @@
+import operator as oper
+
+# Funkcija za izracun:
+def calculator():
+    prvi = input("Molim vas unesite cijeli broj: ")
+    drugi = input("Molim vas unesite cijeli drugi broj: ")
+    operacija = input("Molim vas unesite operaciju +, -, * ili /")
+
+    if operacija == '+':
+        rezultat = int(prvi) + int(drugi)
+        print(rezultat)
+    elif operacija == '-':
+        rezultat = int(prvi) - int(drugi)
+        print(rezultat)
+    elif operacija == '*':
+        rezultat = int(prvi) * int(drugi)
+        print(rezultat)
+    else:
+        rezultat = int(prvi) / int(drugi)
+        print(rezultat)
+    
+    def odlukaPonoviti():
+        odluka = input("Želite li napraviti ponovnu kalkulaciju? (y/n)")
+        if odluka == 'y':
+            calculator()
+        elif odluka == 'n':
+            exit()
+        else:
+            print(" Unujeli ste pogrsni znak: " + odluka)
+            odlukaPonoviti()
+
+    odlukaPonoviti()
+
+
+
+def provjeriOperatora(operacija):
+    return {
+        '+': oper.add,
+        '-': oper.sub,
+        '*': oper.mul,
+        '/': oper.truediv,
+    }[operacija]
+
+calculator()
